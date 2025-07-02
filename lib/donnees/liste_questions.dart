acd07c5786fbeeae514a708e51ffa180d916575e
Samuel <kouaekra@gmail.com>
2025-07-02 08:23:22 +0000

    Ajout de questions

--- lib/donnees/liste_questions.dart
+++ lib/donnees/liste_questions.dart
@@ -14,4 +14,44 @@ final List<Map<String, dynamic>> questions = [
     'choix': ['8', '15', '10', '5'],
     'bonneReponse': '15',
   },
+  {
+    'texte': 'Quelle est la couleur du ciel par temps clair ?',
+    'choix': ['Rouge', 'Vert', 'Bleu', 'Jaune'],
+    'bonneReponse': 'Bleu',
+  },
+  {
+    'texte': 'Quel est l\'élément chimique dont le symbole est O ?',
+    'choix': ['Or', 'Oxygène', 'Hydrogène', 'Carbone'],
+    'bonneReponse': 'Oxygène',
+  },
+  {
+    'texte': 'Qui a écrit "Les Misérables" ?',
+    'choix': ['Victor Hugo', 'Émile Zola', 'Gustave Flaubert', 'Marcel Proust'],
+    'bonneReponse': 'Victor Hugo',
+  },
+  {
+    'texte': 'Quelle est la planète la plus proche du Soleil ?',
+    'choix': ['Terre', 'Mars', 'Vénus', 'Mercure'],
+    'bonneReponse': 'Mercure',
+  },
+  {
+    'texte': 'Quel est le plus grand océan du monde ?',
+    'choix': ['Atlantique', 'Indien', 'Arctique', 'Pacifique'],
+    'bonneReponse': 'Pacifique',
+  },
+  {
+    'texte': 'Quel est le symbole chimique de l\'eau ?',
+    'choix': ['H2O', 'CO2', 'O2', 'NaCl'],
+    'bonneReponse': 'H2O',
+  },
+  {
+    'texte': 'Qui a peint la Joconde ?',
+    'choix': [
+      'Vincent van Gogh',
+      'Pablo Picasso',
+      'Léonard de Vinci',
+      'Claude Monet',
+    ],
+    'bonneReponse': 'Léonard de Vinci',
+  },
 ];
